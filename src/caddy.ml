1220482e0bffc8a89b67219f72efa2ece55464f9
Anil Madhavapeddy <anil@recoil.org>
2020-11-03 14:09:13 +0000

    switch the ocaml.org deployments over to docker-compose and Caddy
    This is because the Swarm-based services cannot bind to a specific
    IP address on the node, and we prefer to not use a front-end
    load balancer on the ocaml.org deployments

---
+++ src/caddy.ml
@@ -0,0 +1,48 @@
+(* Compose configuration for a Caddy HTTPS server on a particular domain/ip *)
+
+type t = {
+  name: string;
+  domain: string;
+  ip: string;
+}
+
+let caddy_service { name; domain; ip; _ } =
+  let service = Fmt.strf {|
+  %s:
+    image: $IMAGE_HASH
+    command: --domain %s --root /usr/share/caddy
+    restart: always
+    networks:
+    - %s_network
+    ports:
+    - target: 80
+      published: 80
+      protocol: tcp
+    - target: 443
+      published: 443
+      protocol: tcp
+|} name domain name
+  in
+  let network = Fmt.strf {|
+  %s_network:
+    driver_opts:
+        com.docker.network.bridge.host_binding_ipv4: "%s"
+|} name ip
+  in
+  service, network
+
+let compose ~sites () =
+  sites |>
+  List.map caddy_service |>
+  List.fold_left (fun (s,n) (service, network) -> (s ^ service), (n ^ network)) ("","") |>
+  fun (services, networks) ->
+  Fmt.strf {|
+version: "3.7"
+services:%s
+networks:%s
+|} services networks
+
+(* Replace $IMAGE_HASH in the compose file with the fixed (hash) image id *)
+let replace_hash_var ~hash contents =
+  Re.Str.(global_replace (regexp_string "$IMAGE_HASH") hash contents)
+
